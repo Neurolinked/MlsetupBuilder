2c81e45d7a394b350f1e7729a8be6cdd2fd43ee6
Neurolinked <neurolinked@gmail.com>
2024-06-07 12:53:01 +0200

    New normal map rebuild for less that 4 channel sources

--- js/workers/imgWork.js
+++ js/workers/imgWork.js
@@ -11,33 +11,82 @@ onmessage = function(event){
       break;
     case 'normalFix':
     default:
-      var workerResult = nMapFix(datas[0],datas[1],datas[2],datas[3],datas[4]);
+      var recalcChannels = 4;
+      if (datas[0].length!=(datas[1]*datas[2]*4)){
+        recalcChannels = datas[0].length/(datas[1]*datas[2]);
+      }
+      var workerResult = nMapFix(datas[0],datas[1],datas[2],datas[3],datas[4],recalcChannels);
       break;
   }
 }
 
 function clamp( value, min, max ) {
-
 	return Math.max( min, Math.min( max, value ) );
+}
 
+function levelling(data,lvl1=0,lvl2=1){
+  if (lvl1 > lvl2){
+    //Swap values
+    [lvl1, lvl2] = [lvl2, lvl1];
+  }
+  lvl1= lvl1<0?0:lvl1;
+  lvl2= lvl2>1?1:lvl2;
+
+  levelBlack = lvl1 * 255;
+  levelWhite = lvl2 * 255;
+
+  data = data < levelBlack ? 0 : data;
+  data = data > levelWhite ? levelWhite : data;
+  data = 255 * ( (data - levelBlack) /(levelWhite - levelBlack));
+  return parseInt(data);
 }
 
 /* This function will write the blue channel of the image as full blue, to trasform
 the red green normal maps to sull rgb */
-function nMapFix(normaldatas,width,height,fileNAME,material){
+function nMapFix(normaldatas,width,height,fileNAME,material,channels){
+  console.log(normaldatas);
   //Took the arraybuffer color change the clue channel
   var red,green,blueVal,blue,alpha;
-  for (let i = 0, l = normaldatas.length; i < l; i += 4) {
-    // Modify pixel data
-    red = (normaldatas[i]/255)* 2 - 1;
-    green = -1 * (((normaldatas[i + 1])/255)* 2 - 1);
-    blueVal = Math.sqrt(1 - Math.pow(red,2) - Math.pow(green,2)); //recalculated floating point
-    blue = parseInt((( blueVal + 1 ) / 2 ) * 255)
-    //blue = 255 // old concept, not recalculated
-    normaldatas[i + 2] = blue;
+  var newTextureData = new Uint8Array(width*height*4);
+
+  switch (channels) {
+    case 2:
+      var kcounter = 0;
+      for (let i = 0, l = normaldatas.length; i < l; i += 2) {
+        // Modify pixel data
+        red = (normaldatas[i]/255)* 2 - 1;
+        green = -1 * (((normaldatas[i + 1])/255)* 2 - 1);
+        blueVal = Math.sqrt(1 - Math.pow(red,2) - Math.pow(green,2)); //recalculated floating point
+        blue = parseInt((( blueVal + 1 ) / 2 ) * 255)
+        //blue = 255 // old concept, not recalculated
+
+        newTextureData[kcounter]=normaldatas[i];
+        newTextureData[kcounter+1]=normaldatas[i+1];
+        newTextureData[kcounter+2]=blue;
+        newTextureData[kcounter+3]=255;
+        kcounter+=4;
+      }    
+      self.postMessage(['interface','rebuilding 2 channel normal map']);
+      break;
+    default:
+      for (let i = 0, l = normaldatas.length; i < l; i += 4) {
+        // Modify pixel data
+        red = (normaldatas[i]/255)* 2 - 1;
+        green = -1 * (((normaldatas[i + 1])/255)* 2 - 1);
+        blueVal = Math.sqrt(1 - Math.pow(red,2) - Math.pow(green,2)); //recalculated floating point
+        blue = parseInt((( blueVal + 1 ) / 2 ) * 255)
+        //blue = 255 // old concept, not recalculated
+        newTextureData[i]=normaldatas[i];
+        newTextureData[i+1]=normaldatas[i+1];
+        newTextureData[i+2]=blue;
+        newTextureData[i+3]=normaldatas[i+3];
+      }
+      self.postMessage(['interface','rebuilding blue channel normal map']);
+      break;
   }
 
-  self.postMessage(['paint',normaldatas,width,height,fileNAME,material])
+
+  self.postMessage(['paint',newTextureData,width,height,fileNAME,material])
 }
 
 function roughnessFix(roughnessdatas,width,height,fileNAME,material){
@@ -49,10 +98,10 @@ function roughnessFix(roughnessdatas,width,height,fileNAME,material){
     green = roughnessdatas[i+1];
     blue = roughnessdatas[i+2];
     //roughnessdatas[i]=green;
-    roughnessdatas[i+1]=red;
+    roughnessdatas[i+1] = red;
   }
-
-  self.postMessage(['rough',roughnessdatas,width,height,fileNAME,material])
+  self.postMessage(['interface','swapping red/green roughness map channels']);
+  self.postMessage(['rough',roughnessdatas,width,height,fileNAME,material]);
 }
 
 function textAlphaFix(texturedatas,width,height,fileNAME,alphaValue=1){
