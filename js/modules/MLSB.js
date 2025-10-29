9b8f0b8e09b6ab08bac4b61eb08d46ad7a5f7647
Neurolinked <neurolinked@gmail.com>
2025-10-29 20:22:51 +0100

    method addition

--- js/modules/MLSB.js
+++ js/modules/MLSB.js
@@ -3,9 +3,9 @@
  * Reference to JSDoc to document the code https://jsdoc.app/tags-property.html
  * @class
  * @property {object} Editor - object that contains property on the UI/UX part of MLSB
+ * @property {number} Editor.activeMlsetup - get or set the selected mask layer
  * @property {number} Editor.layerSelected - get or set the selected mask layer
  * @property {object} Editor.Clone - information container manage cloning
- * @property {object} Editor.Clone.layerProperty - Class Layer information to clone to
  * @property {object} Materials - content of the material DB
  * @property {array} MlSetups - array of the current loaded mlsetups
  * @property {object} TreeD - object that contains property about the 3d environment
@@ -13,13 +13,15 @@
  * @property {string} TreeD.appearance - the appearance the editor is working on
  * @property {object} TreeD.model - the glb scene model loader from Threejs
  * @property {bool} Key.shiftPress - check on software keypressed for behaviours
+ * @property {object} UI - contains all the UI configuration for the Editor
+ * @property {bool} UI.ready - active when the layer system is ready
+ * @property {substance} UI.substance - track the presence of Substance Layer in the UI
  */
 class MLSBEditor {
     Editor = {
+        activeMlsetup : 0,
         layerSelected : 0,
-        Clone : {
-            layerProperty: new Layer()
-        }
+        Clone : new Layer()
     }
 
     TreeD = {
@@ -44,6 +46,11 @@ class MLSBEditor {
 
     MlSetups = []
     Models = []
+
+    /** @constructs */
+	constructor(){
+		this.MlSetups[0] = new Mlsetup;
+	}
     /**
      * Reset the params to the default state
      */
@@ -90,12 +97,30 @@ class MLSBEditor {
         return false
     }
 
+    updMLLayer(mlsetupIndex,layerIndex,layer){
+        try {
+            if ((0 <= mlsetupIndex) && (mlsetupIndex < this.MlSetups.length)){
+                if (this.MlSetups[mlsetupIndex].Layers[layerIndex] instanceof Layer){
+                    this.MlSetups[mlsetupIndex].Layers[layerIndex] = layer;
+                    return true;
+                }else{
+                    throw new Error(`layer,${layerindex} is not a layer object`);        
+                }
+            }else{
+                throw new Error(`Out of actual loaded mlsetups (${this.MlSetups.length})`);
+            }
+        }catch(error){
+            console.error(error);
+            return false;
+        }
+    }
+
     updMlsetup(index,mlsetup,layerIndex){
         try {
             if (mlsetup instanceof Mlsetup){
                 if ((0 <= index) && (index < this.MlSetups.length)){
-                    if (this.MlSetups[index].layers[layerIndex] instanceof Layer){
-                        this.Mlsetups[index].layers[layerIndex] = mlsetup.layers[layerIndex];
+                    if (this.MlSetups[index].Layers[layerIndex] instanceof Layer){
+                        this.MlSetups[index].Layers[layerIndex] = mlsetup.Layers[layerIndex];
                         return true;
                     }else{
                         throw new Error(`layer,${layerindex} is not a layer object`);        
@@ -107,7 +132,8 @@ class MLSBEditor {
                 throw new Error(`Not a mlsetup object`);
             }
         } catch (error) {
-            console.log(`Removing mlsetup from mlsetup manager: ${error}`);
+            console.log(`Removing mlsetup from mlsetup manager: ${index},${layerIndex}, ${error}`);
+            console.log(mlsetup)
         }
         return false;
     }
