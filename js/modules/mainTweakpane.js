4a0afc1d58c706c61ed41227b371eaba717e48be
Neurolinked <neurolinked@gmail.com>
2026-07-22 11:41:23 +0200

    webgpu disable switch transparency

--- js/modules/mainTweakpane.js
+++ js/modules/mainTweakpane.js
@@ -61,9 +61,9 @@ TDtabManager.pages[0].addBinding(PARAMS, 'wireframes',{label:'Display wireframes
   $("#thacanvas").trigger("theWire");
 });
 
-TDtabManager.pages[0].addBinding(PARAMS, 'switchTransparency',{label:'Transparent vs MaskAlpha'}).on('change',(ev)=>{
+/* TDtabManager.pages[0].addBinding(PARAMS, 'switchTransparency',{label:'Transparent vs MaskAlpha'}).on('change',(ev)=>{
   //$("#thacanvas").trigger('switchAlpha');
-});
+}); */
 TDtabManager.pages[0].addBinding(PARAMS, 'maskChannel',{min:0.0,max:1.0, step:0.01, label:'Mask layer opacity\n(only 3d viewport)'}).on('change',(ev)=>{
   $("#thacanvas").trigger('maskAlpha');
 });
